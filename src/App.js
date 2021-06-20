41401e51e840bf4cc79ca49cfb08a6f781ee65a0
tlwns5598 <tlwns5598@gmail.com>
2021-06-21 08:34:21 +0900

    pharse added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>HELLO WORLD!!</h1>
+      <p>another phrase</p>
     </div>
   );
 }
